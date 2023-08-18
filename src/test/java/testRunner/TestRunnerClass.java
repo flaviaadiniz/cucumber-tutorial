4c97aea8ff155fe0a98815fbdd9b4cffe24bba0f
flaviaadiniz <flaviaadiniz@gmail.com>
2023-08-18 15:24:57 -0300

    Added dryRun and monochrome options on RunnerClass - (for study purposes only)

--- src/test/java/testRunner/TestRunnerClass.java
+++ src/test/java/testRunner/TestRunnerClass.java
@@ -8,8 +8,9 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = {"src/test/resources/features"}, //path to the directory where feature files are
         glue = {"steps"}, //path to the directory where step definitions classes are
-        plugin = {"pretty", "junit: Report2"}, // specifies different formatting options for the output reports
-        dryRun = true //finds out if there are any scenarios or steps that don't have step definitions associated
+        plugin = {"pretty", "junit: Report2"}// specifies different formatting options for the output reports
+        // dryRun = true - finds out if there are any scenarios or steps that don't have associated step definitions
+        // monochrome = true - makes console output for the tests more readable and remove any unreadable character
 )
 public class TestRunnerClass {
 }
