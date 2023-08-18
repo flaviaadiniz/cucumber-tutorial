a8130265e483d3cb252b701e19fe33b4c725f8cb
flaviaadiniz <flaviaadiniz@gmail.com>
2023-08-18 15:11:42 -0300

    Added plugin and dryRun options on TestRunner class

--- src/test/java/testRunner/TestRunnerClass.java
+++ src/test/java/testRunner/TestRunnerClass.java
@@ -6,8 +6,10 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = {"src/test/resources/features"},
-        glue = {"steps"}
+        features = {"src/test/resources/features"}, //path to the directory where feature files are
+        glue = {"steps"}, //path to the directory where step definitions classes are
+        plugin = {"pretty", "junit: Report2"}, // specifies different formatting options for the output reports
+        dryRun = true //finds out if there are any scenarios or steps that don't have step definitions associated
 )
 public class TestRunnerClass {
 }
